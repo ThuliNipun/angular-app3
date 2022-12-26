4081d3b6072b19e77e97ca44a78e929306d86024
Thulitha Nipun <nthulitha@gmail.com>
2022-12-26 23:01:37 +0530

    paramMap Scroll without reloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponent } from './component/customer/customer.component';
+import { ItemComponent } from './component/item/item.component';
+import { OrderComponent } from './component/order/order.component';
+import { PlaceOrderComponent } from './component/place-order/place-order.component';
+import {NotFoundPageComponent} from "./component/not-found-page/not-found-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/customer',pathMatch:'full'},
+  {path:'customer/:name', component:CustomerComponent},
+  {path:'item', component:ItemComponent},
+  {path:'order', component:OrderComponent},
+  {path:'place-order', component:PlaceOrderComponent},
+  {path:'**',component:NotFoundPageComponent} //should be last
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './component/customer/customer.component';
+import { ItemComponent } from './component/item/item.component';
+import { OrderComponent } from './component/order/order.component';
+import { PlaceOrderComponent } from './component/place-order/place-order.component';
+import { NotFoundPageComponent } from './component/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    ItemComponent,
+    OrderComponent,
+    PlaceOrderComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
